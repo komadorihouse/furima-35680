0a6e5e7aacac23a430cb94386bc95984badac84f
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-03 17:04:47 +0900

    itemsテーブルの作成・設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :user
+    validates :product_name
+    validates :product_descrip
+    validates :product_status_id
+    validates :shipping_charge_id
+    validates :region_id
+    validates :shipping_period_id
+    validates :category_id
+    validates :price
+  end
 end
--- db/migrate/20210603055841_create_items.rb
+++ db/migrate/20210603055841_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.references :user ,               null: false, foreign_key: true
+      t.string     :product_name ,       null: false 
+      t.text       :product_descrip ,    null: false 
+      t.integer    :product_status_id ,  null: false 
+      t.integer    :shipping_charge_id , null: false
+      t.integer    :region_id ,          null: false
+      t.integer    :shipping_period_id , null: false
+      t.integer    :category_id ,        null: false
+      t.integer    :price ,              null: false
       t.timestamps
     end
   end
