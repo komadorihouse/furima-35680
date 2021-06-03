b01767f9462277fa9e150a180feff177cd51205d
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-03 19:05:56 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,23 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :user
-    validates :product_name
-    validates :product_descrip
-    validates :product_status_id
-    validates :shipping_charge_id
-    validates :region_id
-    validates :shipping_period_id
-    validates :category_id
-    validates :price
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :genre
+
+    validates :user,              presence: true
+    validates :product_name ,     presence: true,     length: { minimum: 1, maximum: 40 }
+    validates :product_descrip,   presence: true,     length: { minimum: 1, maximum: 1000 }
+    validates :category_id,     numericality: { other_than: 0 } 
+    #validates :shipping_charge_id
+    #validates :region_id
+    #validates :shipping_period_id
+    #validates :product_status_id
+    #validates :price
+    #validates :image
+
+  belongs_to :user
+  has_one_attached :image
+
+  
+  
+
+
 end
