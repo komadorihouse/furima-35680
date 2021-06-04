3d8e19497f61eaef3c0fd85e42ea412cc4e465a6
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-05 01:27:24 +0900

    Rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
     validates :product_name,         length: { minimum: 1, maximum: 40 }
     validates :product_descrip,      length: { minimum: 1, maximum: 1000 }
     validates :price,
-                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
   with_options numericality: { other_than: 0 } do
     validates :category_id
@@ -20,9 +20,8 @@ class Item < ApplicationRecord
     validates :shipping_period_id
   end
 
-  validates :region_id,           numericality: { other_than: 1 }
-  
-  
+  validates :region_id, numericality: { other_than: 1 }
+
   belongs_to :user
   has_one_attached :image
 end
