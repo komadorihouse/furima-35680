cd4f8d816db7443d16a21ae85fa3c64467b23027
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-04 14:03:36 +0900

    商品出品情報保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:product_name, :product_descrip, :category_id, :product_status_id, :shipping_charge_id, :region_id, :shipping_period_id, :price, :image).merge(user_id: current_user.id)
   end
 end
