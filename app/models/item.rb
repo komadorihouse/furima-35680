26ca1ebe464278e2fd721dd5d46daa550ac5330c
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-04 11:52:12 +0900

    販売価格、手数料、利益計算実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :genre
+    belongs_to :status
+    belongs_to :shipping
+    belongs_to :region
+    belongs_to :period
 
-    validates :user,              presence: true
-    validates :product_name ,     presence: true,     length: { minimum: 1, maximum: 40 }
-    validates :product_descrip,   presence: true,     length: { minimum: 1, maximum: 1000 }
-    validates :category_id,     numericality: { other_than: 0 } 
-    #validates :shipping_charge_id
-    #validates :region_id
-    #validates :shipping_period_id
-    #validates :product_status_id
-    #validates :price
-    #validates :image
+    validates :user,                 presence: true
+    validates :product_name ,       presence: true,     length: { minimum: 1, maximum: 40 }
+    validates :product_descrip,     presence: true,     length: { minimum: 1, maximum: 1000 }
+    validates :category_id,         numericality: { other_than: 0 } 
+    validates :product_status_id,   numericality: { other_than: 0 } 
+    validates :shipping_charge_id,  numericality: { other_than: 0 } 
+    validates :region_id,           numericality: { other_than: 0 } 
+    validates :shipping_period_id,  numericality: { other_than: 0 } 
+    validates :price ,               presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :image,               presence: true
 
   belongs_to :user
   has_one_attached :image
