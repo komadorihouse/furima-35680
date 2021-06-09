ca5de9072d2dc6de1f326dbbfa753f4c8374b959
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-09 11:39:27 +0900

    Formオブジェクトにバリテーションの設定
    subscribersモデルのバリテーションを削除

--- app/models/order_subscribers.rb
+++ app/models/order_subscribers.rb
@@ -1,5 +1,15 @@
 class OrderSubscribers
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :order_id, :postal_code, :region_id, :city, :street, :building_name, :phone
-  
+
+  with_options presende: true do
+    validates :user_id
+    validates :item_id
+    validates :order_id
+    validates :postal_code
+    validates :region_id
+    validates :city
+    validates :street
+    validates :phone
+  end
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,9 +1,3 @@
 class Subscriber < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :region_id
-    validates :city
-    validates :street
-    validates :phone
-  end
+  
 end
