23c44ae01dd4ac2286ecf403d74bc423f1a50ffb
komadorihouse <komadorihouse@yahoo.co.jp>
2021-06-10 16:53:42 +0900

    出品者自身の商品購入ページ遷移を無効化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index 
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
     @order_subscribers = OrderSubscribers.new
   end
 
